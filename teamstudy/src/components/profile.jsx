6152168e07a5067436935e3c11087219bed433b8
Lakde Maheshwari <maheshwarilakde@gmail.com>
2025-01-15 09:45:33 +0530

    Adding the reward page

---
+++ teamstudy/src/components/profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className="flex flex-col justify-center items-center mt-6">
+        <div className="m-5 p-5 border-gray-700 bg-gray-800 border-2 rounded-md w-2/4">
+            <h1 className="text-red-600">Profile</h1>
+        </div>
+        </div>
+    );
+}
+
+export default Profile;
