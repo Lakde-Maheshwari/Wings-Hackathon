b3a89b09870b4c3c3af452d3471491ea1f3264cf
Lakde Maheshwari <maheshwarilakde@gmail.com>
2025-01-15 16:07:35 +0530

    Added reward coin

--- teamstudy/src/context/socketProvider.jsx
+++ teamstudy/src/context/socketProvider.jsx
@@ -6,14 +6,17 @@ const socketContext = createContext(null);
 // Hook
 export const useSocket = () => {
   const socket = useContext(socketContext);
+  console.log(socket);
   return socket;
 };
 
-export const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("http://localhost:5000"), []); // Memoize the socket instance
+const SocketProvider = ({ children }) => {
+  const socket = useMemo(() => io("http://localhost:8000"), []); // Memoize the socket instance
   return (
     <socketContext.Provider value={socket}>
       {children}
     </socketContext.Provider>
   );
 };
+
+export default SocketProvider;
